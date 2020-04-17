21a236581d97dd5fb464e257c05bd7d5cf8d3b4d
Killian <castillon.killian@gmail.com>
2020-04-17 21:11:19 +0200

    Changed default serviceTag (rendezvous string) : doesn't seem to handle spaces well

--- config/config.go
+++ config/config.go
@@ -13,7 +13,7 @@ type Config struct {
 func ParseFlags() (Config, error) {
 	config := Config{}
 	flag.IntVar(&config.ListenPort, "port", 3000, "Port to listen to")
-	flag.StringVar(&config.RendezvousString, "rendezvous", "meet me here",
+	flag.StringVar(&config.RendezvousString, "rendezvous", "rendezvouspoint",
 		"Unique string to identify group of nodes. Share this with your friends to let them connect with you")
 	flag.StringVar(&config.ProtocolID, "pid", "/chat/1.1.0", "Sets a protocol id for stream headers")
 	flag.Parse()
